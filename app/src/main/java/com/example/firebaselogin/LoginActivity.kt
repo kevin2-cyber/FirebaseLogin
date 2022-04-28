a9019a8a7ba6c40722666e068cb338e26a39e53c
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-28 17:58:40 +0000

    setting up project

--- app/src/main/java/com/example/firebaselogin/LoginActivity.kt
+++ app/src/main/java/com/example/firebaselogin/LoginActivity.kt
@@ -3,9 +3,9 @@ package com.example.firebaselogin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
