89329ce2c31548bfea505cffb719f98dc22facb6
kevin2-cyber <keduful@st.ug.edu.gh>
2022-05-03 00:05:56 +0000

    name

--- app/src/main/java/com/example/firebaselogin/WelcomeActivity.kt
+++ app/src/main/java/com/example/firebaselogin/WelcomeActivity.kt
@@ -2,10 +2,16 @@ package com.example.firebaselogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
+//        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_welcome)
     }
 }
